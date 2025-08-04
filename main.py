4b73539e313129a85fbc6b2c0e6d4370f602ef2d
han16nah <han_nah@mailbox.org>
2025-08-04 09:07:31 +0200

    revert DistributedDataParallel

--- main.py
+++ main.py
@@ -60,7 +60,8 @@ if __name__ == '__main__':
     config.model = Pipeline(config)
     if torch.cuda.device_count() > 1 and config.gpu_mode:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
-        config.model = torch.nn.parallel.DistributedDataParallel(config.model)
+        # config.model = torch.nn.parallel.DistributedDataParallel(config.model)
+        config.model = torch.nn.DataParallel(config.model)
     # config.model = KPFCNN(config)
 
     # create optimizer 
